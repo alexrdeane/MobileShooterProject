694ded242105d9d2cc1bbcee2a1a163836b424a0
Alex Deane <alexrdeane@hotmail.com>
2019-08-05 14:22:03 +1000

    initial commit

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public JoyStick movementJoystick;
+    public JoyStick rotationJoystick;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        transform.position += transform.TransformDirection(new Vector3(0f, 0f, movementJoystick.Vertical) * 2f * Time.deltaTime);
+        transform.rotation *= Quaternion.Euler(0f , Mathf.Abs(movementJoystick.Horizontal) > .5 ? movementJoystick.Horizontal : 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AI
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Variables
+        [Header("Variables: ")]
+        //player's movement speed
+        public float movementSpeed = 5f;
+        public float movementThreshold;
+        //player's private rigidbody
+        private Rigidbody rigid;
+        public JoyStick joystick;
+        #endregion
+
+        #region Start
+        void Start()
+        {
+            //gets the rigid body from the player
+            rigid = GetComponent<Rigidbody>();
+        }
+        #endregion
+
+        #region Update
+        void Update()
+        {
+            Move();
+        }
+        #endregion
+
+        void Move()
+        {
+            Vector2 input = new Vector2(joystick.Horizontal, joystick.Vertical);
+            if (input.x * input.x + input.y * input.y < movementThreshold * movementThreshold)
+            {
+                input = input.normalized * 0.001f;
+            }
+            else
+            {
+                input = input.normalized;
+            }
+
+            rigid.velocity = new Vector3(input.x, 0f, input.y) * movementSpeed;
+            if (input != Vector2.zero)
+            {
+                transform.forward = rigid.velocity;
+            }
+        }
+    }
+}
+
