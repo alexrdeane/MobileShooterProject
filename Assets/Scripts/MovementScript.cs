48003c113150c5c243523d9edfd7b9d49eb1bc43
Alex Deane <alexrdeane@hotmail.com>
2019-08-05 23:15:43 +1000

    added materials

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -6,6 +6,7 @@ public class MovementScript : MonoBehaviour
 {
     public JoyStick movementJoystick;
     public JoyStick rotationJoystick;
+    public float runSpeed = 2f, rotateSpeed = 50f;
 
     void Start()
     {
@@ -15,7 +16,8 @@ public class MovementScript : MonoBehaviour
     void Update()
     {
 
-        transform.position += transform.TransformDirection(new Vector3(0f, 0f, movementJoystick.Vertical) * 2f * Time.deltaTime);
-        transform.rotation *= Quaternion.Euler(0f , Mathf.Abs(movementJoystick.Horizontal) > .5 ? movementJoystick.Horizontal : 0f, 0f);
+        transform.position += transform.TransformDirection(new Vector3(0f, 0f, movementJoystick.Vertical) * runSpeed * Time.deltaTime);
+        //transform.rotation *= Quaternion.Euler(0f , movementJoystick.Horizontal * rotateSpeed * Time.deltaTime, 0f);
+        transform.rotation *= Quaternion.Euler(0f, Mathf.Abs(movementJoystick.Horizontal) > .5 ? movementJoystick.Horizontal * rotateSpeed : 0f, 0f);
     }
 }
