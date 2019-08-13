671bd0799be5a57f3aeb0a921071516fd028c176
Alex Deane <alexrdeane@hotmail.com>
2019-08-13 15:35:02 +1000

    two joystick

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -8,16 +8,24 @@ public class MovementScript : MonoBehaviour
     public JoyStick rotationJoystick;
     public float runSpeed = 2f, rotateSpeed = 50f;
 
+    public Transform cam;
+
+    Vector3 rot;
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-
-        transform.position += transform.TransformDirection(new Vector3(0f, 0f, movementJoystick.Vertical) * runSpeed * Time.deltaTime);
+        rot += new Vector3(-rotationJoystick.Vertical, rotationJoystick.Horizontal, 0f) * rotateSpeed * Time.deltaTime;
+        rot.x = Mathf.Clamp(rot.x, -90f, 90f);
+        transform.position += transform.TransformDirection(new Vector3(movementJoystick.Horizontal, 0f, movementJoystick.Vertical) * runSpeed * Time.deltaTime);
         //transform.rotation *= Quaternion.Euler(0f , movementJoystick.Horizontal * rotateSpeed * Time.deltaTime, 0f);
-        transform.rotation *= Quaternion.Euler(0f, Mathf.Abs(movementJoystick.Horizontal) > .5 ? movementJoystick.Horizontal * rotateSpeed : 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, rot.y, 0f);
+        cam.localRotation = Quaternion.Euler(rot.x, 0f, 0f);
     }
+
+
 }
