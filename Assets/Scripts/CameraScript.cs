45aa68fda0978ca10c326d0b82feeb945b21e8da
Alex Deane <alexrdeane@hotmail.com>
2019-08-20 21:23:42 +1000

    added other rooms and camera script

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public LayerMask door;
+    public Transform cam;
+    void Update()
+    {
+        
+    }
+
+    public void ChangeRoom()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 0.3f, door))
+        {
+            Debug.Log("ye");
+        }
+    }
+}
